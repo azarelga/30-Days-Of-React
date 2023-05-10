3ded449e13f3e7ececace9246c241986880a1b2d
Azarel <skootydack@gmail.com>
2023-05-10 11:35:16 +0700

    day1 started

---
+++ day-1/array_exercise.js
@@ -0,0 +1,97 @@
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+]
+
+// Declare an empty array;
+const arr = []
+
+// Declare an array with more than 5 number of elements
+arr = [1, 2, 3, 4, 5]
+
+// Find the length of your array
+console.log(arr.length())
+
+// Get the first item, the middle item and the last item of the array
+console.log(arr[0], arr[arr.length() / 2], arr[arr.length() - 1])
+
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = ["Azarel", 19, True, "Katalia", 2003]
+
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = [
+  "Facebook", "Google",
+  "Microsoft", "Apple", "IBM", "Oracle", "Amazon"
+]
+
+// Print the array using console.log()
+console.log(itCompanies)
+
+// Print the number of companies in the array
+console.log(itCompanies.length())
+
+// Print the first company, middle and last company
+console.log(itCompanies[0], itCompanies[itCompanies.length() / 2], itCompanies[itCompanies.length() - 1])
+
+// Print out each company
+for (let index = 0; index < array.length; index++) {
+  console.log(itCompanies[index]);
+}
+
+// Change each company name to uppercase one by one and print them out
+for (let index = 0; index < array.length; index++) {
+  itCompanies[index].toUpperCase();
+  console.log(itCompanies[index]);
+}
+
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+for (let index = 0; index < array.length; index++) {
+  console.log(itCompanies[index]);
+}
+console.log("are big IT companies");
+
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
+
+// Filter out companies which have more than one 'o' without the filter method
+
+
+// Sort the array using sort() method
+const sortedCompanies = itCompanies.sort()
+
+// Reverse the array using reverse() method
+
+// Slice out the first 3 companies from the array
+
+// Slice out the last 3 companies from the array
+
+// Slice out the middle IT company or companies from the array
+
+// Remove the first IT company from the array
+
+// Remove the middle IT company or companies from the array
+
+// Remove the last IT company from the array
+
+// Remove all IT companies
+
+
---
+++ day-1/variables.js
@@ -0,0 +1,5 @@
+let firstName = "Azarel"
+firstName = 'Grahandito'
+
+const PI = 3.14 // Not allowed to reassign PI to new value
+console.log(PI)
